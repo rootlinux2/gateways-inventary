712f1d977cfee229833f1e2f43d802a962610852
Yoandry Collazo <ycbarrosocu@gmail.com>
2021-01-30 22:59:00 -0500

    👌 IMPROVE: remove unnecesary code

--- Components/Spinner/index.js
+++ Components/Spinner/index.js
@@ -1,4 +1,3 @@
-import React from "react";
 import styled, { keyframes, ThemeProvider } from "styled-components";
 
 const rotate360 = keyframes`
@@ -9,20 +8,6 @@ const rotate360 = keyframes`
     transform: rotate(360deg);
   }
 `;
-
-// const Spin = styled.div`
-//   animation: ${rotate360} 1s linear infinite;
-//   transform: translateZ(0);
-
-//   border-top: 2px solid grey;
-//   border-right: 2px solid grey;
-//   border-bottom: 2px solid grey;
-//   border-left: 4px solid black;
-//   background: transparent;
-//   width: 24px;
-//   height: 24px;
-//   border-radius: 50%;
-// `;
 
 const Spin = styled.div`
   animation: ${rotate360} 1s linear infinite;
@@ -38,33 +23,22 @@ const Spin = styled.div`
   border-radius: 50%;
 `;
 
-Spin.defaultProps = {
-  theme: {
-    bt: "2px solid grey",
-    br: "2px solid grey",
-    bb: "2px solid grey",
-    bl: "4px solid black",
-    radius: "24px",
-  },
-};
-
-// Define what props.theme will look like
 
 
 const Spinner = ({ bgColor, fgColor, radius }) => {
   const theme = {
-    bt: `2px solid ${bgColor?bgColor:'grey'}`,
-    br: `2px solid ${bgColor?bgColor:'grey'}`,
-    bb: `2px solid ${bgColor?bgColor:'grey'}`,
-    bl: `4px solid ${fgColor?fgColor:'black'}`,
-    radius: radius?radius:'24px',
+    bt: `2px solid ${bgColor ? bgColor : "grey"}`,
+    br: `2px solid ${bgColor ? bgColor : "grey"}`,
+    bb: `2px solid ${bgColor ? bgColor : "grey"}`,
+    bl: `4px solid ${fgColor ? fgColor : "black"}`,
+    radius: radius ? radius : "24px",
   };
-  console.log(theme);
   return (
     <ThemeProvider theme={theme}>
       <Spin />
     </ThemeProvider>
   );
 };
+
 
 export default Spinner;
